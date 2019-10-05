de966f1e0f66ec1ac708e42a1839c87a1a89e0ae
radnaasuren <radnaasuren@NEST-18s-Mac-mini.local>
2019-10-05 18:33:57 +0800

    login.html login.js login.css 100% DONE!!!

--- source/js/login.js
+++ source/js/login.js
@@ -1,59 +1,65 @@
-document.getElementsByClassName("rectangle-2")[0].style.display = "none";
-document.getElementsByClassName('rectangle-3')[0].style.display="none";
-document.getElementsByClassName('withLogin')[0].style.display="none";
-firebase.auth().onAuthStateChanged(function(user) {
+
+firebase.auth().onAuthStateChanged(function (user) {
+    console.log('user: ', user);
     if (user) {
-        // playerId = user;
-        // console.log(playerId)
-        player = user;
+        playerId = user;
+        console.log(playerId)
+        player = user; 
+        document.getElementsByClassName("googleSignIn")[0].classList.add('hide');
+        document.getElementsByClassName('withoutLogin')[0].classList.add('hide');
+        document.getElementsByClassName('rectangle-2')[0].classList.add('hide');
+        document.getElementsByClassName('withLogin')[0].classList.remove('hide');
         document.getElementsByClassName('info')[0].classList.remove('hide');
-        document.getElementsByClassName('googleSignIn')[0].classList.add('hide');
+        document.getElementsByClassName('rectangle-2')[0].style.display = 'none';
         document.getElementById('username').innerHTML = user.displayName.split(' ')[0];
+        document.getElementById("papa").style.justifyContent ='space-between';
+
+        // document.getElementsByClassName('info').style.display='flex';
+        
         rooms.get()
-            .then(function(querySnapshot) {
-                querySnapshot.forEach(function(doc) {
+            .then(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
                     let room = doc.data();
-                    room.players.forEach(function(p) {
+                    room.players.forEach(function (p) {
                         if (p.uid == player.uid) {
                             roomId = doc.id;
-                            //waitingRoom(roomId);
+                            waitingRoom(roomId);
                         }
                     });
                 });
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.log("error", error);
             });
 
     } else {
+        document.getElementsByClassName('withoutLogin')[0].classList.remove('hide');
+        document.getElementsByClassName('withLogin')[0].classList.add('hide');
         document.getElementsByClassName('info')[0].classList.add('hide');
-        document.getElementsByClassName('googleSignIn')[0].classList.remove('hide');
+        document.getElementsByClassName('rectangle-2')[0].classList.add('hide');
+        document.getElementsByClassName('rectangle-2')[0].style.display = 'none';
     }
 });
 
 function gmail() {
-    document.getElementsByClassName("withoutLogin")[0].classList.add('hide');
-    document.getElementsByClassName("googleSignIn")[0].classList.add('hide');
-    document.getElementsByClassName("rectangle-2")[0].style.display = "flex";
-    // document.getElementsByClassName("rectangle-2")[0].style.displa
-    document.getElementsByClassName("rectangle-2")[0].style.justifyContent = "space-between";
-    document.getElementById("papa").style.justifyContent = "flex-start";
-    console.log("mailshu")
+   document.getElementsByClassName('withoutLogIn')[0].classList.add('hide');
+   document.getElementsByClassName('googleSignIn')[0].classList.add('hide');
+   document.getElementsByClassName('rectangle-2')[0].classList.remove('hide');
+   document.getElementsByClassName('rectangle-2')[0].style.display = 'flex';
+   document.getElementById("papa").style.justifyContent = "flex-start";
 }
 
 
 
 function login() {
     var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
+    firebase.auth().signInWithPopup(provider).then(function (result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
-        document.getElementsByClassName('withLogin')[0].style.display="flex";
-        document.getElementsByClassName('withoutLogin')[0].style.display='none';
         // ...
-    }).catch(function(error) {
+    }).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -67,37 +73,43 @@ function login() {
 
 }
 
-<<<<<<< HEAD
 
 function signOutpage() {
+    document.getElementsByClassName('withLogin')[0].classList.add('hide');
     document.getElementsByClassName('info')[0].classList.add('hide');
-    document.getElementsByClassName("withLogin")[0].classList.add('hide');
-    document.getElementsByClassName('withoutLogin')[0].classList.add('hide');
-    document.getElementsByClassName('rectangle-3')[0].style.display="flex"; 
+    document.getElementsByClassName('rectangle-3')[0].classList.remove('hide');
+    document.getElementsByClassName('rectangle-3')[0].style.display='flex';
+    document.getElementsByClassName('rectangle-2')[0].classList.add('hide');
+    document.getElementsByClassName('rectangle-2')[0].style.display = 'none';
 }
-        
+
 function realsignout() {
-=======
-function signOut() {
->>>>>>> 042b697814a4e791918ed7ae1f1aadad5a1dd0fa
-    firebase.auth().signOut().then(function() {
+    firebase.auth().signOut().then(function () {
         // Sign-out successful.
         console.log('Signed out!');
-    }).catch(function(error) {
+    }).catch(function (error) {
         // An error happe ned.
         console.log('Error occurred!');
     });
     document.getElementsByClassName('info')[0].classList.add('hide');
+    document.getElementsByClassName('rectangle-3')[0].classList.add('hide');
+    document.getElementsByClassName('rectangle-3')[0].style.display='none';
+    document.getElementsByClassName('WithLogin')[0].classList.add('hide');
+    document.getElementsByClassName('withoutLogIn')[0].classList.remove('hide');
     document.getElementsByClassName('googleSignIn')[0].classList.remove('hide');
-    document.getElementsByClassName('rectangle-3')[0].style.display="none";
-    document.getElementsByClassName('withLogin')[0].classList.add('hide');
-    document.getElementsByClassName('withoutLogin')[0].classList.remove('hide');
-
+    document.getElementsByClassName('rectangle-2')[0].classList.add('hide');
 }
-function back(){
-    document.getElementsByClassName('googleSignIn')[0].classList.add('hide');
+function back() {
     document.getElementsByClassName('info')[0].classList.remove('hide');
-    console.log("BaCK!!!!");
-    document.getElementsByClassName('rectangle-3')[0].style.display="none";
-    // document.getElementsByClassName('withLogin')[0].style.display="true";
+    document.getElementsByClassName('rectangle-3')[0].classList.add('hide');
+    document.getElementsByClassName('rectangle-3')[0].style.display='none';
+    document.getElementsByClassName('WithLogin')[0].classList.remove('hide');
+    document.getElementsByClassName('rectangle-2')[0].classList.add('hide');
+}
+function back1() {
+    document.getElementsByClassName('rectangle-2')[0].style.display = 'none';
+    document.getElementById("papa").style.justifyContent ='space-between';
+    document.getElementsByClassName('rectangle-2')[0].classList.add('hide');
+    document.getElementsByClassName('withoutLogin')[0].classList.remove('hide');
+    document.getElementsByClassName("googleSignIn")[0].classList.remove('hide');
 }
